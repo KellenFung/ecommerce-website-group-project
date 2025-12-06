336fc32afc632111f70eaf8c4f4972d3e52170e7
Kellen <157562490+KellenFung@users.noreply.github.com>
2025-12-06 02:06:27 -0500

    Update main.py

--- electriccommerce/backend/main.py
+++ electriccommerce/backend/main.py
@@ -421,6 +421,6 @@ register_payment_routes(app, pool, JWT_SECRET, PAYMENT_ENCRYPTION_KEY)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
 
-    app.run(host="0.0.0.0", port=port, debug=True)
 
