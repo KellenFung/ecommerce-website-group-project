2fed3277527343a92c35f2907d0c30062e5fccaa
Kellen <157562490+KellenFung@users.noreply.github.com>
2025-12-07 22:16:21 -0500

    Update login-edit.js

--- docs/js/login-edit.js
+++ docs/js/login-edit.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000/api";
+const API = "https://ecommerce-website-group-project-production-e0ec.up.railway.app/api";
 
 // Get JWT token from localStorage
 function getAuthToken() {
@@ -182,3 +182,4 @@ document.addEventListener("DOMContentLoaded", () => {
   loadAccountInfo();
   setupSaveButtons();
 });
+
